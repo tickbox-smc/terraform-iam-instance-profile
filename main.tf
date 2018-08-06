8c2aaec24cb130a82ec6bcc9cf55ad2d9601b405
Chris Falck <chris.falck@tickboxconsulting.com>
2018-08-06 16:07:16 +0100

    new version as added s3:DeleteObject to profile

--- main.tf
+++ main.tf
@@ -129,6 +129,7 @@ resource "aws_iam_role_policy" "ssm_policy" {
             "Action": [
                 "s3:PutObject",
                 "s3:GetObject",
+                "s3:DeleteObject",
                 "s3:AbortMultipartUpload",
                 "s3:ListMultipartUploadParts",
                 "s3:ListBucket",
